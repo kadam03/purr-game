5767d957ea5e1c7467296561698e6592aa7f1775
kadam03 <62893104+kadam03@users.noreply.github.com>
2024-09-24 00:59:51 +0200

    4 different furball with different size

---
+++ furballFusion/Script/ball.gd
@@ -0,0 +1,30 @@
+@tool
+extends RigidBody2D
+
+@export var ball_value: int = 0
+@export var ball_size: float = 1.0
+@export var ball_texture: Texture2D
+
+@onready var sprite = $Sprite
+@onready var collision_shape = $CollisionShape2D
+#
+func _ready() -> void:
+	sprite.texture = ball_texture
+	sprite.scale = Vector2(ball_size, ball_size)
+	collision_shape.scale = sprite.scale
+	update_ball_properties()
+	
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		update_ball_properties()
+
+# Update the ball's visual and physical properties
+func update_ball_properties() -> void:
+	if sprite and ball_texture:
+		sprite.texture = ball_texture
+		sprite.scale = Vector2(ball_size, ball_size)
+	
+	# Adjust the collision shape to match the new size
+	if collision_shape and collision_shape.shape is CircleShape2D:
+		var radius = (sprite.texture.get_size().x / 2)
+		collision_shape.shape.radius = radius
--- furballFusion/Script/game_area.gd
+++ furballFusion/Script/game_area.gd
@@ -5,11 +5,11 @@ var ball
 var mouse_pressed: bool = false
 @onready var lim_left = $SpawnLimiters/LimiterLeft
 @onready var lim_right = $SpawnLimiters/LimiterRight
+@export var balls: Array[PackedScene]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 var pos_x
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,23 +21,15 @@ func _process(delta: float) -> void:
 		if pos_x > lim_right.position.x:
 			pos_x = lim_right.position.x
 		ball.position.x = pos_x	
-		
-#func _process(delta: float) -> void:
-	#if mouse_pressed and ball != null:
-		#var pos_x = get_viewport().get_mouse_position().x
-		## Constrain position within limits
-		#pos_x = clamp(pos_x, lim_left.position.x, lim_right.position.x)
-		## Move the ball along the X-axis only
-		#ball.position.x = pos_x
 
 func _input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed("mouse_click") and ball == null:
 		mouse_pressed = true
-		ballscene = load("res://Scene/Ball.tscn")
+		#ballscene = load("res://Scene/Ball.tscn")
+		ballscene = balls[randi() % balls.size()]
 		ball = ballscene.instantiate()
 		ball.position.y = lim_left.position.y
-		#ball.position = Vector2(get_viewport().get_mouse_position().x, lim_left.position.y)
 		ball.freeze = true
 		add_child(ball)
 	
