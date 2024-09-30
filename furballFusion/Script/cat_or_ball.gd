68be64ec23dab4450d3c5dab9f02619ec55f7bb9
Guber Tamas <gut1bp@bosch.com>
2024-09-30 17:13:20 +0200

    adding new things

--- furballFusion/Script/cat_or_ball.gd
+++ furballFusion/Script/cat_or_ball.gd
@@ -3,17 +3,20 @@ extends RigidBody2D
 const MAX_INIT_INDEX: int  = 2
 const SCORE_MULTIPLIER:int  = 10
 const IMAGE_SCALE_CONST:int = 30
-const KABUM_DRUATION: float = 0.4
+const KABUM_DRUATION: float = 0.6
 const VIBRATION_DURATION: int = 200
 
 @onready var cats_and_balls: AnimatedSprite2D = $cats_and_balls
 @onready var merge_sound: AudioStreamPlayer = $merge_sound
+@onready var kabum: AnimatedSprite2D = $kabum
 
 
-var collision_polygon: CollisionPolygon2D
+
+#var collision_polygon: CollisionPolygon2D
 var unit_score: int = 0
 var ball_animations: Array[StringName] = []
 var cat_animations: Array[StringName] = []
+var angry_animations: Array[StringName] = []
 var number_of_textures:int
 var current_id:int = 0
 var current_type: String 
@@ -26,6 +29,8 @@ func init_variables():
 			cat_animations.append(animation_name)
 		elif animation_name.begins_with("ball"):
 			ball_animations.append(animation_name)
+		elif animation_name.begins_with("angry"):
+			angry_animations.append(animation_name)
 	current_id=(randi_range(0,MAX_INIT_INDEX))
 	current_type=["cat","ball","ball"][randi_range(0,2)]
 	
@@ -52,8 +57,12 @@ func add_or_replace_collision_polygon():
 	for child in get_children():
 		if child is CollisionPolygon2D:
 			child.queue_free()
-	collision_polygon=create_collision_polygon()
-	add_child(collision_polygon)
+	var offset=get_animationTexture().get_size()*cats_and_balls.scale
+	var collision_polygon2=create_collision_polygon()
+	#call_deferred("add_child",collision_polygon2)
+	#collision_polygon2.call_deferred("position",cats_and_balls.position - offset/2)
+	collision_polygon2.position = cats_and_balls.position - offset/2
+	add_child(collision_polygon2)
 
 
 	
@@ -78,21 +87,21 @@ func set_animation():
 func update_animation_properties():
 	unit_score += (current_id + 1) * SCORE_MULTIPLIER
 	cats_and_balls.scale=get_animation_scale()
-	var offset=get_animationTexture().get_size()*cats_and_balls.scale
 	add_or_replace_collision_polygon()
-	collision_polygon.position = cats_and_balls.position - offset/2
 	self.mass =float(current_id + 1)
 	add_to_group(str(current_id))
 	
 func play_kabumm():
-	add_to_group("kabum")
+	cats_and_balls.self_modulate.a=0.2
 	var kabumm_scale = (current_id+2)*IMAGE_SCALE_CONST/512.0
-	cats_and_balls.scale=Vector2(kabumm_scale,kabumm_scale)
-	cats_and_balls.play("kabum")
+	kabum.visible=true
+	kabum.scale=Vector2(kabumm_scale,kabumm_scale)
+	kabum.play("kabum")
 	merge_sound.play()
 	Input.vibrate_handheld(VIBRATION_DURATION)
 	await get_tree().create_timer(KABUM_DRUATION).timeout
-	remove_from_group("kabum")
+	kabum.visible=false
+	cats_and_balls.self_modulate.a=1
 	
 
 func _ready():
@@ -104,15 +113,17 @@ func _ready():
 
 
 func _on_Ball_collided(ball):
-	if  ball is RigidBody2D and ball.current_type == "ball" and self.current_type == "cat" and ball.is_in_group(str(current_id)) and current_id <= MAX_INIT_INDEX:
-		var new_position = (self.position + ball.position) / 2
-		Global.score+=ball.unit_score+self.unit_score
-		Global.set_new_highscore()
-		ball.queue_free()
-		self.position = new_position
-		remove_from_group(str(current_id))
-		current_id+=1
-		current_type="cat"
-		await  play_kabumm()
-		set_animation()
-		update_animation_properties()
+	if  ball is RigidBody2D and ball.is_in_group(str(current_id)):
+		if ball.current_type == "ball" and current_id <= MAX_INIT_INDEX:
+			var new_position = (self.position + ball.position) / 2
+			Global.score+=ball.unit_score+self.unit_score
+			Global.set_new_highscore()
+			ball.queue_free()
+			self.position = new_position
+			remove_from_group(str(current_id))
+			current_id+=1
+			play_kabumm()
+			set_animation()
+			update_animation_properties()
+		elif ball.current_type == "cat" and self.current_type == "cat":
+			cats_and_balls.play(angry_animations[current_id])
